51397a4cd6dcf7573dc5122443be7fe79d230d17
Hamidov Ali <hemidoveli893@gmail.com>
2026-07-10 01:04:57 +0400

    refactor: remove unnecessary things

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.routes.api import router
 
 app = FastAPI(title="DATASCIENCE RAG BOT API")
 
-@app.get("/health_check", summary = "Check server's status")
+@app.get("/health_check", summary = "Check server status")
 def health_check():
     return {"status": "online", "message": "API is running!"}
 
